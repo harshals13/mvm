2b62103582725cb740f19f90c4209db86eb68af4
Harsha Suryavamshi <harsha@minebrat.com>
2019-07-01 22:13:02 +0530

    Inputting data

--- Player.java
+++ Player.java
@@ -1,5 +1,28 @@
 public class Player {
     private String playerName;
 
-    private String playerScore;
+    private int playerScore;
+
+	public Player(String name, int score) {
+		// TODO Auto-generated constructor stub
+		this.playerName = name;
+		
+		this.playerScore =score;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getPlayerScore() {
+		return playerScore;
+	}
+
+	public void setPlayerScore(int playerScore) {
+		this.playerScore = playerScore;
+	}
 }
